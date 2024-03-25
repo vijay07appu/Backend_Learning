4b8c9e4995f4457bcfe1b7d1168b740e76b1cddd
Vijay <vijay07appu@gmail.com>
2024-03-25 13:43:11 +0530

    all set up configured

--- src/app.js
+++ src/app.js
@@ -29,4 +29,15 @@ app.use(cookieParser())
 
 
 
+
+// routes import 
+
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration 
+
+app.use("/api/v1/users",userRouter)
+
+
+
 export {app}
